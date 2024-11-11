a1e30f66fe612da53199ea17f944ea598c5b8267
Alaa El-Ebshihy <alaa.el-ebshihy@researchstudio.at>
2024-11-11 13:11:42 +0100

    Changed the fewshots to be dynamic

--- src/nkd/llm/nkd_generator.py
+++ src/nkd/llm/nkd_generator.py
@@ -2,11 +2,26 @@
 Generator for Narrative Knowledge Delta given a pair of scientific papers using specific LLM
 """
 
+import ast
+
 import pandas as pd
 
 from src.nkd.llm.input_paper_text_constructor import construct_paper_input_text
 from src.nkd.llm.llm_api_caller import get_llm_response
 from src.utils.io_util import *
+
+APPROACH_FEW_SHOTS = """
+  - Article 1 uses FRRN approach for Real-Time Semantic Segmentation, while Article 2 uses Dilation10 for Real-Time Semantic Segmentation.
+  - Article 1 uses Mapillary approach for Semantic Segmentation, while Article 2 uses ParseNet  for Semantic Segmentation.
+"""
+
+DATA__FEW_SHOTS = """
+  - Article 1 and Article 2 use Cityscapes dataset.
+  - Article 1 uses multiple datasets:  CamVid, and  ADE20K, while Article 2 uses PASCAL Context dataset.
+  - Article 1 uses multiple datasets:  CamVid,  PASCAL VOC 2012, and  ADE20K, while Article 2 does not use specific datasets for experiments.
+  - Article 1 uses PASCAL VOC 2012 dataset, while Article 2 does not use specific datasets for experiments.
+  - Article 1 uses Cityscapes dataset, while Article 2 uses PASCAL Context dataset.
+"""
 
 
 def get_annotation_path(annotation_dir, row, pair_no):
@@ -14,6 +29,29 @@ def get_annotation_path(annotation_dir, row, pair_no):
     if f'arxiv_id_{pair_no}' in row:
         annotation_path = join(annotation_dir, str(row[f'arxiv_id_{pair_no}']) + '.json')
     return annotation_path
+
+
+def convert_fewshots_to_string(diffs):
+    for index, diff in enumerate(diffs):
+        diff = diff.replace("sent:", "  -", 1)
+        diff = diff.replace("sent:", "\r\n  -")
+        diffs[index] = diff
+    return "\r\n".join(diffs)
+
+
+def construct_prompts_fewshots(pairs_row, prompt_template):
+    syn_datasets_diffs = ast.literal_eval(pairs_row['syn_datasets_diff'])
+    syn_approach_diffs = ast.literal_eval(pairs_row['syn_approach_diff'])
+    dataset_fewshots = DATA__FEW_SHOTS
+    approach_fewshots = APPROACH_FEW_SHOTS
+    if len(syn_datasets_diffs) != 0:
+        dataset_fewshots = convert_fewshots_to_string(syn_datasets_diffs)
+    if len(syn_approach_diffs) != 0:
+        approach_fewshots = convert_fewshots_to_string(syn_approach_diffs)
+    prompt = prompt_template
+    prompt = prompt.replace('{approach_few_shots_text}', approach_fewshots)
+    prompt = prompt.replace('{data_few_shots_text}', dataset_fewshots)
+    return prompt
 
 
 def construct_prompt(prompt_template, paper_1_input_text, paper_2_input_text):
@@ -53,8 +91,8 @@ def append_to_claims(claims, doc_ids, sents, cur_claim_id):
     return cur_claim_id
 
 
-def construct_claims(papers_pairs_info, input_text_type, annotation_dir, prompt_template, llm_api_type, llm_model,
-                     llm_api_key):
+def construct_claims(papers_pairs_info, input_text_type, annotation_dir, prompt_template, is_few_shots, llm_api_type,
+                     llm_model, llm_api_key):
     datasets_claims = []
     approach_claims = []
     split_keydifferences_claims = []
@@ -69,10 +107,17 @@ def construct_claims(papers_pairs_info, input_text_type, annotation_dir, prompt_
         annotation_path_2 = get_annotation_path(annotation_dir, row, 2)
         if not path_exits(annotation_path_1) or not path_exits(annotation_path_2):
             continue
+
+        prompt = prompt_template
+
+        print(is_few_shots)
+        if is_few_shots:
+            prompt = construct_prompts_fewshots(row, prompt)
+
         paper_1_input_text = construct_paper_input_text(input_text_type, annotation_path_1)
         paper_2_input_text = construct_paper_input_text(input_text_type, annotation_path_2)
 
-        prompt = construct_prompt(prompt_template, paper_1_input_text, paper_2_input_text)
+        prompt = construct_prompt(prompt, paper_1_input_text, paper_2_input_text)
         try:
             gpt_response = get_llm_response(llm_api_type, prompt, llm_model, llm_api_key)
             if gpt_response.startswith("```json"):
@@ -108,13 +153,12 @@ def construct_claims(papers_pairs_info, input_text_type, annotation_dir, prompt_
     return datasets_claims, approach_claims, split_keydifferences_claims, raw_keydifferences_claims, raw_output
 
 
-def generate_claims(papers_pairs_info, claims_base_path, input_text_type, annotation_dir, prompt_template, llm_api_type,
-                    llm_model, llm_api_key, run_start=1, run_end=2):
-
+def generate_claims(papers_pairs_info, claims_base_path, input_text_type, annotation_dir, prompt_template, is_few_shots,
+                    llm_api_type, llm_model, llm_api_key, run_start=1, run_end=2):
     for run in range(run_start, run_end):
         datasets_claims, approach_claims, split_keydifferences_claims, raw_keydifferences_claims, raw_output = construct_claims(
-            papers_pairs_info,
-            input_text_type, annotation_dir, prompt_template, llm_api_type, llm_model, llm_api_key)
+            papers_pairs_info, input_text_type, annotation_dir, prompt_template, is_few_shots, llm_api_type, llm_model,
+            llm_api_key)
         merged_claims = datasets_claims + approach_claims + split_keydifferences_claims
         run_claims_dir = join(claims_base_path, f'{llm_model}/run_{run}')
         makedirs(run_claims_dir)
--- src/scripts/nkd/llm/nkd_generator_executer.py
+++ src/scripts/nkd/llm/nkd_generator_executer.py
@@ -5,19 +5,20 @@ from src.utils.io_util import *
 
 
 def main(args):
-    papers_pairs_info = join(args.papers_base_dir, 'papers_pairs.csv')
-    claims_base_path = join(args.papers_base_dir, 'claims')
+    papers_pairs_info = join(args.papers_base_dir, 'papers_pairs_fewshots.csv')
+    claims_base_path = join(args.papers_base_dir, f'claims/{args.experiment_name}')
     input_text_type = args.input_text_type
     annotation_dir = join(args.papers_base_dir, args.annotation_dir)
     prompt_template = '\r\n'.join(read_file_into_list(args.prompt_template_path))
     llm_api_type = args.llm_api_type
     llm_model = args.llm_model
     llm_api_key = args.llm_api_key
-    run_start = args.run_start
-    run_end = args.run_end
+    is_few_shots = args.is_few_shots == 'True'
+    run_start = int(args.run_start)
+    run_end = int(args.run_end)
 
-    generate_claims(papers_pairs_info, claims_base_path, input_text_type, annotation_dir, prompt_template, llm_api_type,
-                    llm_model, llm_api_key, run_start=run_start, run_end=run_end)
+    generate_claims(papers_pairs_info, claims_base_path, input_text_type, annotation_dir, prompt_template, is_few_shots,
+                    llm_api_type, llm_model, llm_api_key, run_start=run_start, run_end=run_end)
 
 
 if __name__ == "__main__":
@@ -25,10 +26,12 @@ if __name__ == "__main__":
     parser.add_argument("papers_base_dir", help="The base directory that has paper information and to save output in.")
     parser.add_argument("input_text_type", help="full or az.")
     parser.add_argument("annotation_dir", help="The directory of the parsed paper text, it depends on the input type.")
+    parser.add_argument("experiment_name", help="The directory of the parsed paper text, it depends on the input type.")
     parser.add_argument("prompt_template_path", help="Text file of the prompt to be used.")
     parser.add_argument("llm_api_type", help="Api type used to generate the output: openai or together.")
     parser.add_argument("llm_model", help="Key referring to the LLM: gpt4o, gpt4o_mini, llama31-8b, or llama31-70b.")
     parser.add_argument("llm_api_key", help="API key.")
+    parser.add_argument("is_few_shots", help="Boolean to indicate if the prompt is few shots or not.", default=False)
     parser.add_argument("run_start", default=1)
     parser.add_argument("run_end", default=5)
 
